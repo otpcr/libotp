60604c9cad08f81b962fabf9c0ae9759a085a507
Bart Thate <rssbotd@gmail.com>
2024-09-23 06:27:32 +0200

    add event and config

---
+++ otp/config.py
@@ -0,0 +1,31 @@
+# This file is placed in the Public Domain.
+
+
+import os
+
+
+from .object  import Default
+from .persist import Workdir
+
+
+class Config(Default):
+
+    "Config"
+
+    name = Default.__module__.split(".", maxsplit=2)[-2]
+    wdr = os.path.expanduser(f"~/.{name}")
+    pidfile = os.path.join(wdr, f"{name}.pid")
+
+    def __init__(self, name=None):
+        self.name = name or Config.name
+        self.wdr = Config.wdr
+        self.pidfile = Config.pidfile
+
+
+Workdir.wdr = Config.wdr
+
+
+def __dir__():
+    return (
+        'Config',
+    )
